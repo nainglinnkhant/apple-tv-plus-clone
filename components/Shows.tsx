855632c782d766b302fdcb25ced16b25d8cc4925
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-03-09 13:44:02 +0630

    fix: fix infinite marquee

--- components/Shows.tsx
+++ components/Shows.tsx
@@ -1,64 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
 
 import { SHOWSLISTONE, SHOWSLISTTWO } from '../data'
 import styles from '../styles/Shows.module.scss'
 
 const Shows = () => {
-  const [marqueeOneDuration, setMarqueeOneDuration] = useState(42)
-  const [marqueeTwoDuration, setMarqueeTwoDuration] = useState(35)
-
-  const slowDownMarqueeOne = () => {
-    setMarqueeOneDuration(150)
-  }
-
-  const slowDownMarqueeTwo = () => {
-    setMarqueeTwoDuration(150)
-  }
-
-  const resetMarqueeOne = () => {
-    setMarqueeOneDuration(42)
-  }
-
-  const resetMarqueeTwo = () => {
-    setMarqueeTwoDuration(35)
-  }
-
-  useEffect(() => {
-    document
-      .getElementById('marquee-1')
-      ?.addEventListener('mouseenter', slowDownMarqueeOne)
-    document
-      .getElementById('marquee-2')
-      ?.addEventListener('mouseenter', slowDownMarqueeTwo)
-    document.getElementById('marquee-1')?.addEventListener('mouseleave', resetMarqueeOne)
-    document.getElementById('marquee-2')?.addEventListener('mouseleave', resetMarqueeTwo)
-
-    return () => {
-      document
-        .getElementById('marquee-1')
-        ?.removeEventListener('mouseenter', slowDownMarqueeOne)
-      document
-        .getElementById('marquee-2')
-        ?.removeEventListener('mouseenter', slowDownMarqueeTwo)
-      document
-        .getElementById('marquee-1')
-        ?.removeEventListener('mouseleave', resetMarqueeOne)
-      document
-        .getElementById('marquee-2')
-        ?.removeEventListener('mouseleave', resetMarqueeTwo)
-    }
-  }, [])
-
   return (
     <section className={styles['shows-section']}>
       <div className={styles['shows-marquee']}>
-        <div
-          className={styles['marquee-wrapper']}
-          style={{ animationDuration: `${marqueeOneDuration}s` }}
-        >
-          <ul>
-            {SHOWSLISTONE.slice(0, 5).map(({ poster }, index) => (
+        <div className={styles['marquee-wrapper']}>
+          <ul className={styles['marquee-one']}>
+            {SHOWSLISTONE.map(({ poster }, index) => (
               <li key={index} style={{ backgroundImage: `url(${poster})` }}>
                 <Link href='/' />
 
@@ -67,8 +19,10 @@ const Shows = () => {
                 <button>Stream now</button>
               </li>
             ))}
+          </ul>
 
-            {SHOWSLISTONE.slice(5).map(({ poster }, index) => (
+          <ul className={styles['marquee-one']}>
+            {SHOWSLISTONE.map(({ poster }, index) => (
               <li key={index} style={{ backgroundImage: `url(${poster})` }}>
                 <Link href='/' />
 
@@ -80,12 +34,9 @@ const Shows = () => {
           </ul>
         </div>
 
-        <div
-          className={styles['marquee-wrapper']}
-          style={{ animationDuration: `${marqueeTwoDuration}s` }}
-        >
-          <ul>
-            {SHOWSLISTTWO.slice(0, 5).map(({ poster }, index) => (
+        <div className={styles['marquee-wrapper']}>
+          <ul className={styles['marquee-two']}>
+            {SHOWSLISTTWO.map(({ poster }, index) => (
               <li key={index} style={{ backgroundImage: `url(${poster})` }}>
                 <Link href='/' />
 
@@ -94,8 +45,10 @@ const Shows = () => {
                 <button>Stream now</button>
               </li>
             ))}
+          </ul>
 
-            {SHOWSLISTTWO.slice(5).map(({ poster }, index) => (
+          <ul className={styles['marquee-two']}>
+            {SHOWSLISTTWO.map(({ poster }, index) => (
               <li key={index} style={{ backgroundImage: `url(${poster})` }}>
                 <Link href='/' />
 
